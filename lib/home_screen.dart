d90c11cc91c3a6e39442764b0c049addd74a218c
Bruno Glauco Eles <bgeles@gmail.com>
2020-08-19 15:28:32 -0300

    att

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,22 +1,64 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'BRUNO GLAUCO ELES',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+      body: Container(
+        color: Colors.deepPurpleAccent,
+        child: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(width: 20.0, height: 100.0),
+              Text(
+                "Be",
+                style: TextStyle(fontSize: 43.0),
+              ),
+              SizedBox(width: 20.0, height: 100.0),
+              RotateAnimatedTextKit(
+                  onTap: () {
+                    print("Tap Event");
+                  },
+                  text: ["AWESOME", "OPTIMISTIC", "DIFFERENT"],
+                  textStyle: TextStyle(fontSize: 40.0, fontFamily: "Horizon"),
+                  textAlign: TextAlign.start,
+                  alignment:
+                      AlignmentDirectional.topStart // or Alignment.topLeft
+                  ),
+            ],
           ),
         ),
-        backgroundColor: Colors.black45,
-      ),
-      body: Container(
-        color: Colors.yellowAccent,
       ),
     );
   }
+}
+
+Widget animateText() {
+  return Row(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      SizedBox(width: 20.0, height: 100.0),
+      Text(
+        "Be",
+        style: TextStyle(fontSize: 43.0),
+      ),
+      SizedBox(width: 20.0, height: 100.0),
+      RotateAnimatedTextKit(
+          onTap: () {
+            print("Tap Event");
+          },
+          text: ["AWESOME", "OPTIMISTIC", "DIFFERENT"],
+          textStyle: TextStyle(fontSize: 40.0, fontFamily: "Horizon"),
+          textAlign: TextAlign.start,
+          alignment: AlignmentDirectional.topStart // or Alignment.topLeft
+          ),
+    ],
+  );
 }
